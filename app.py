ee926407141b2e800040eb99afdc7fdb21f4f08e
Omar Fakhreddine <fakhredo@flip1.engr.oregonstate.edu>
2021-05-23 21:16:53 -0700

    added routes for all pages

--- app.py
+++ app.py
@@ -52,6 +52,78 @@ def exercises():
     # Sends the results back to the web browser.
     return render_template("exercises.j2")
 
+    
+@app.route('/muscle_groups')
+def muscle_groups():
+    # Write the query and save it to a variable
+    #query = "SELECT * FROM MuscleGroups;"
+
+    # The way the interface between MySQL and Flask works is by using an
+    # object called a cursor. Think of it as the object that acts as the
+    # person typing commands directly into the MySQL command line and
+    # reading them back to you when it gets results
+    #cursor = db.execute_query(db_connection=db_connection, query=query)
+
+    # The cursor.fetchall() function tells the cursor object to return all
+    # the results from the previously executed
+    #
+    # The json.dumps() function simply converts the dictionary that was
+    # returned by the fetchall() call to JSON so we can display it on the
+    # page.
+
+    #results = cursor.fetchall()
+
+    # Sends the results back to the web browser.
+    return render_template("muscle_groups.j2")
+
+    
+@app.route('/movement_types')
+def movement_types():
+    # Write the query and save it to a variable
+    #query = "SELECT * FROM MovementTypes;"
+
+    # The way the interface between MySQL and Flask works is by using an
+    # object called a cursor. Think of it as the object that acts as the
+    # person typing commands directly into the MySQL command line and
+    # reading them back to you when it gets results
+    #cursor = db.execute_query(db_connection=db_connection, query=query)
+
+    # The cursor.fetchall() function tells the cursor object to return all
+    # the results from the previously executed
+    #
+    # The json.dumps() function simply converts the dictionary that was
+    # returned by the fetchall() call to JSON so we can display it on the
+    # page.
+
+    #results = cursor.fetchall()
+
+    # Sends the results back to the web browser.
+    return render_template("movement_types.j2")
+
+    
+@app.route('/training_types')
+def training_types():
+    # Write the query and save it to a variable
+    #query = "SELECT * FROM TrainingTypes;"
+
+    # The way the interface between MySQL and Flask works is by using an
+    # object called a cursor. Think of it as the object that acts as the
+    # person typing commands directly into the MySQL command line and
+    # reading them back to you when it gets results
+    #cursor = db.execute_query(db_connection=db_connection, query=query)
+
+    # The cursor.fetchall() function tells the cursor object to return all
+    # the results from the previously executed
+    #
+    # The json.dumps() function simply converts the dictionary that was
+    # returned by the fetchall() call to JSON so we can display it on the
+    # page.
+
+    #results = cursor.fetchall()
+
+    # Sends the results back to the web browser.
+    return render_template("training_types.j2")
+
     # Listener
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 4082))
