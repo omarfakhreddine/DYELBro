e05187e9ed3aa371367ab9f8820bf8af1f1d2dee
Douglas <bonillad@oregonstate.edu>
2021-05-26 00:59:55 -0700

    working

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from flask import request, redirect
 import os
 import json
 import database.db_connector as db
@@ -13,6 +14,8 @@ SELECT [columns] FROM [table] (WHERE [condition]) (ORDER BY [column] ASC/DESC) (
 """
 TODO: write queries for each table/form
 TODO: create for loops in templates to handle query results
+TODO: make database entities have unique requirement
+TODO: make search functional by making search input into forms
 """
 
 # Configuration
@@ -28,124 +31,119 @@ def root():
     return render_template("home.j2")
 
 
-
-
-
 @app.route('/exercises', methods=['POST', 'GET'])
 def exercises():
-    #print("testing")
+    
     headings = ("Exercise ID", "Exercise Name", "Training Type", "Movement Type", "Muscle Groups")
-    data = (
+    placeholder = (
     ("1", "Bench Press", "Strength", "Push", "Chest, Arms"),
     ("2", "Deadlift", "Strength", "Pull", "Back"),
     ("3", "Romanian Squat", "Strength", "Squat", "Legs"),
     ("4", "Bicep Curl", "Strength", "Pull", "Arms")
     )
 
-
-
-
-
-    #if request.method == 'POST':
-        #task_content = request.form['']
-    #else:
-        #return render_template("exercises.j2", headings=headings, data=data)
-
-
-    # Write the query and save it to a variable
-    #query = "SELECT * FROM Exercises;"
-    
-    # The way the interface between MySQL and Flask works is by using an
-    # object called a cursor. Think of it as the object that acts as the
-    # person typing commands directly into the MySQL command line and
-    # reading them back to you when it gets results
-    #cursor = db.execute_query(db_connection=db_connection, query=query)
-
-    # The cursor.fetchall() function tells the cursor object to return all
-    # the results from the previously executed
-    #
-    # The json.dumps() function simply converts the dictionary that was
-    # returned by the fetchall() call to JSON so we can display it on the
-    # page.
-
+    if request.method == "POST":
+        exercise_name = request.form.get('exercise_name')
+        training = request.form.get('training')
+        movement = request.form.get('movement')
+        muscle_groups = request.form.get('muscle_groups')
+        #query = "INSERT INTO Exercises (exerciseName) VALUES (%s)"
+        print("The exercise name is:", exercise_name) 
+     
+    #query = "INSERT INTO Exercises (exerciseName) VALUES (%s)"
+    #query_args = (exercise_name)
+    #cursor = db.execute_query(db_connection=db_connection, query=query, query_args=query_args)
     #results = cursor.fetchall()
+    get_id = "SELECT exerciseId FROM Exercises WHERE exerciseName = 'bench press' "
+    cursor = db.execute_query(db_connection=db_connection, query=get_id)
+    results_id = cursor.fetchall() #data from database.
 
+    get_ex = "SELECT exerciseName FROM Exercises WHERE exerciseId = 1"
+    cursor = db.execute_query(db_connection=db_connection, query=get_ex)
+    results_ex = cursor.fetchall() #data from database.
+     
+    get_train = "SELECT trainingType FROM TrainingTypes WHERE trainingType = 'cardio' "
+    cursor = db.execute_query(db_connection=db_connection, query=get_train)
+    results_tr= cursor.fetchall() #data from database.
 
-    # Sends the results back to the web browser.
+    get_move = "SELECT movementType FROM MovementTypes WHERE movementType = 'hinge' "
+    cursor = db.execute_query(db_connection=db_connection, query=get_move)
+    results_mov= cursor.fetchall() #data from database.
 
-    return render_template("exercises.j2", headings=headings, data=data)
+    get_musc = "SELECT muscleGroup FROM MuscleGroups WHERE muscleGroup = 'biceps' "
+    cursor = db.execute_query(db_connection=db_connection, query=get_musc)
+    results_musc= cursor.fetchall() #data from database.
+    
+   # return render_template("exercises.j2", headings=headings, data=placeholder)
+    return render_template("exercises.j2", id_num=results_id, ex_num=results_ex, tr_num=results_tr)
+   
 
     
-@app.route('/muscle_groups')
+@app.route('/muscle_groups', methods=['GET', 'POST'])
 def muscle_groups():
-    # Write the query and save it to a variable
-    #query = "SELECT * FROM MuscleGroups;"
+    placeholder = (
+    ("Chest, Arms"), ("Back"), ("Legs"), ("Arms")
+    )
 
-    # The way the interface between MySQL and Flask works is by using an
-    # object called a cursor. Think of it as the object that acts as the
-    # person typing commands directly into the MySQL command line and
-    # reading them back to you when it gets results
-    #cursor = db.execute_query(db_connection=db_connection, query=query)
+    if request.method == 'POST':
 
-    # The cursor.fetchall() function tells the cursor object to return all
-    # the results from the previously executed
-    #
-    # The json.dumps() function simply converts the dictionary that was
-    # returned by the fetchall() call to JSON so we can display it on the
-    # page.
-
-    #results = cursor.fetchall()
-
-    # Sends the results back to the web browser.
-    return render_template("muscle_groups.j2")
+        muscle_groups = request.form.get('muscle_groups')
+        print("The muscle group is", muscle_groups)
+        query = "INSERT INTO MuscleGroups (muscleGroup) VALUES (%s)"
+        query_args = (muscle_groups) 
+        query_results = db.execute_query(db_connection, query, [query_args])
+        db_connection.commit()
 
     
-@app.route('/movement_types')
+    get_data = "SELECT muscleGroup FROM MuscleGroups"
+    cursor = db.execute_query(db_connection=db_connection, query=get_data)
+    results = cursor.fetchall() #data from database.
+        
+
+    return render_template("muscle_groups.j2", data=results)
+    
+@app.route('/movement_types', methods=['GET', 'POST'])
 def movement_types():
-    # Write the query and save it to a variable
-    #query = "SELECT * FROM MovementTypes;"
+    
+    placeholder = (
+    ("hinge"), ("push"), ("pull"), ("flex")
+    )
 
-    # The way the interface between MySQL and Flask works is by using an
-    # object called a cursor. Think of it as the object that acts as the
-    # person typing commands directly into the MySQL command line and
-    # reading them back to you when it gets results
-    #cursor = db.execute_query(db_connection=db_connection, query=query)
+    if request.method == 'POST':
 
-    # The cursor.fetchall() function tells the cursor object to return all
-    # the results from the previously executed
-    #
-    # The json.dumps() function simply converts the dictionary that was
-    # returned by the fetchall() call to JSON so we can display it on the
-    # page.
+        movement_type = request.form.get('movement')
+        print("The movement type is", movement_type)
 
-    #results = cursor.fetchall()
+        query = "INSERT INTO MovementTypes (movementType) VALUES (%s)"
+        query_args = (movement_type) 
+        query_results = db.execute_query(db_connection, query, [query_args])
+        db_connection.commit()
 
-    # Sends the results back to the web browser.
-    return render_template("movement_types.j2")
+    get_data = "SELECT movementType FROM MovementTypes"
+    cursor = db.execute_query(db_connection=db_connection, query=get_data)
+    results = cursor.fetchall() #data from database.
+
+    return render_template("movement_types.j2", data=results)
 
     
-@app.route('/training_types')
+@app.route('/training_types', methods=['GET','POST'])
 def training_types():
-    # Write the query and save it to a variable
-    #query = "SELECT * FROM TrainingTypes;"
+    
+    if request.method == 'POST':
 
-    # The way the interface between MySQL and Flask works is by using an
-    # object called a cursor. Think of it as the object that acts as the
-    # person typing commands directly into the MySQL command line and
-    # reading them back to you when it gets results
-    #cursor = db.execute_query(db_connection=db_connection, query=query)
+        training = request.form.get('training')
+        print("The training type is", training)
 
-    # The cursor.fetchall() function tells the cursor object to return all
-    # the results from the previously executed
-    #
-    # The json.dumps() function simply converts the dictionary that was
-    # returned by the fetchall() call to JSON so we can display it on the
-    # page.
+        query = "INSERT INTO TrainingTypes (trainingType) VALUES (%s)"
+        query_args = (training) 
+        query_results = db.execute_query(db_connection, query, [query_args])
+        db_connection.commit()
 
-    #results = cursor.fetchall()
+    get_data = "SELECT trainingType FROM TrainingTypes"
+    cursor = db.execute_query(db_connection=db_connection, query=get_data)
+    results = cursor.fetchall() #data from database.
 
-    # Sends the results back to the web browser.
-    return render_template("training_types.j2")
+    return render_template("training_types.j2", data=results)
 
     # Listener
 if __name__ == "__main__":
